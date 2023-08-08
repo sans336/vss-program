07593df9fedfbb309496c6383cd1618e44c85485
sans336 <sanskritij76@gmail.com>
2023-08-08 23:08:25 +0530

    new

---
+++ javascript/day3-questions-scopes/questions.js
@@ -0,0 +1,118 @@
+// var x = 10;
+
+// function foo() {
+//   console.log(x);
+// }
+
+// function bar() {
+//   var x = 20;
+//   foo();
+// }
+
+// bar();
+// var x = 10;
+
+// function foo() {
+//   console.log(x);
+//   // foo();
+// }
+
+// function bar() {
+//   x = 20;
+//   // console.log(x)
+//   foo();
+// }
+
+// bar();
+
+// var x = 10;
+
+// function foo() {
+//   console.log(x);
+// }
+
+// function bar() {
+//   x = 20;
+//   console.log(x);
+//   foo();
+// }
+
+// bar();
+// var a = 5;
+
+// function outer() {
+//   var b = 10;
+
+//   function inner() {
+//     var c = 15;
+//     console.log(a + b + c);
+//   }
+
+//   inner();
+// }
+
+// outer();
+
+// var a = 5;
+
+// function outer() {
+//   var b = 10;
+
+//   function inner() {
+//     var c = 15;
+//     console.log(a + b + c);
+//     var a = 10;
+//   }
+
+//   inner();
+// }
+
+// outer();
+// var a = 5;
+
+// function outer() {
+//   var b = 10;
+
+//   function inner() {
+//     var c = 15;
+//     console.log(a + b + c);
+//     let a = 10;
+//   }
+
+//   inner();
+// }
+
+// outer();
+
+// let x = 5;
+
+// function example() {
+//   console.log(x);
+
+//   if (true) {
+//     let x = 10;
+//     const y = 15;
+//     console.log(x + y);
+//   }
+
+//   console.log(x);
+// }
+
+// example();
+// console.log(x);
+// function foo() {
+//   console.log(a);
+//   var a = 10;
+//   console.log(a);
+// }
+
+// foo();
+
+
+// function foo() {
+//   console.log(a);
+//   let a = 10;
+//   console.log(a);
+// }
+
+// foo();
